e5f0860ae0b590bdc21351f9bd4623653c6291b1
Ben.con <041ymoob@gmail.com>
2021-06-07 14:10:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -37,3 +37,34 @@ function displayEmployees(employeeData){
    });
    gridCon.innerHTML = employeeHTML
 }
+
+function displayModal(index){
+    let{ name, dob, phone, email, location:{ city, street, state, postcode},picture,} =employees[index];
+
+    let date = new date(dob.date)
+
+    const modalHtML = `
+    <img class="avatar" src="${picture.large}" alt="">
+    <div class="text-container">
+        <h2 class="name">${name.first} ${name.last}</h2>
+        <p class="email">${email}</p>
+        <p class="address">${city}</p>
+        <hr />
+        <p>${phone}</p>
+        <p class="address"> ${street.name},${state.name},${postcode.name}</p>
+        <p>${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p></p>
+    </div>
+    `
+    overlay.classList.remove('hidden')
+    modalContainer.innerHTML = modalHtML
+}
+
+gridCon.addEventListener('click', e =>  {
+    if (e.target !== gridCon){
+        const card = e.target.closest('.card');
+        const index = card.getAttribute('data-index')
+        displayModal(index)
+    }
+
+})
+
